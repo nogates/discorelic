b6629e3e8f5447f9cfe852975d65cc05646a3e72
David <david.tapiador@envato.com>
2016-08-23 22:18:54 +0200

    getting ready for publishing discorelic to hex.pm

--- mix.exs
+++ mix.exs
@@ -3,13 +3,16 @@ defmodule Discorelic.Mixfile do
 
   def project do
     [
-      app: :discorelic,
-      version: "0.0.1",
-      elixir: "~> 1.2",
-      build_embedded: Mix.env == :prod,
-      start_permanent: Mix.env == :prod,
-      elixirc_paths: elixirc_paths(Mix.env),
-      deps: deps,
+      app:               :discorelic,
+      version:           "0.1.0",
+      elixir:            "~> 1.2",
+      description:       description,
+      package:           package,
+      build_embedded:    Mix.env == :prod,
+      start_permanent:   Mix.env == :prod,
+      elixirc_paths:     elixirc_paths(Mix.env),
+      deps:              deps,
+      test_coverage:     [ tool: ExCoveralls ],
       preferred_cli_env: [
         "coveralls":        :test,
         "coveralls.detail": :test,
@@ -17,7 +20,6 @@ defmodule Discorelic.Mixfile do
         "coveralls.html":   :test,
         "coveralls.travis": :test,
       ],
-      test_coverage: [ tool: ExCoveralls ]
     ]
   end
 
@@ -25,6 +27,22 @@ defmodule Discorelic.Mixfile do
     [
       mod: { Discorelic, [] },
       applications: [ :logger, :lhttpc ]
+    ]
+  end
+
+  defp description do
+    """
+      Elixir implementation of the NewRelic intrumentation PaaS
+    """
+  end
+
+  defp package do
+    [
+      maintainers: ["nogates"],
+      licenses: ["MIT"],
+      links: %{
+        "GitHub" => "https://github.com/nogates/discorelic",
+      }
     ]
   end
 
